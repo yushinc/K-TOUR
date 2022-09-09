b0625c32e08b70ecddc9469bff81d8880e287eb2
Yushin <102500650+yushinc@users.noreply.github.com>
2022-09-10 00:58:26 +0900

    Update urls.py

--- myproject/urls.py
+++ myproject/urls.py
@@ -10,11 +10,15 @@ urlpatterns = [
     path('', views.home, name='home'),
     # 검색 기능
     path('search/', views.search, name='search'), 
-    # 게시판, 글생성, 게시판 상세 페이지, 댓글
+    
+    
+    # (최유신) 게시판, 글생성, 게시판 상세 페이지, 댓글
     path('board/', views.board, name='board'),
     path('postcreate/', views.postcreate, name='postcreate'), 
     path('detail/<int:post_id>', views.detail, name='detail'), 
     path('comment/<int:post_id>', views.comment, name='comment'),
+    
+    
     # 코스 추천 
     path('region/', views.region, name='region'),
     path('jeju/', views.jeju, name='jeju'),
@@ -25,7 +29,7 @@ urlpatterns = [
     path('festivaldetail/', views.festivaldetail, name='festivaldetail'),  
     
     
-    #accounts
+    # (최유신) 로그인, 로그아웃, 
     path('login/', accounts_views.login, name='login'),
     path('logout/', accounts_views.logout, name='logout'),
     path('signup/', accounts_views.signup, name='signup'),
